3865a77103952cdb6d1409b3bd68c400520d6c30
conary <stefande@umich.edu>
2017-12-13 23:11:50 -0500

    Getting more data

--- reddit.py
+++ reddit.py
@@ -65,27 +65,76 @@ STATE = create_state_string(10)
 
 
 
+
+
+
 def get_token():
     client_auth = requests.auth.HTTPBasicAuth(CLIENT_ID, CLIENT_SECRET)
     post_data = {"grant_type": "password", "username": "conary", "password": "bot123"}
     headers = {"User-Agent": "conary/0.1"}
     response = requests.post("https://www.reddit.com/api/v1/access_token", auth=client_auth, data=post_data, headers=headers)
     cred = json.loads(response.text)
-    print(cred)
+    #print(cred)
     return cred['access_token']
     
 
 def get_data(token, url):
-    headers = {"Authorization": "bearer 3E_eJvL2kwpvyKcSGuC8tVkKpo4", "User-Agent": "ChangeMeClient/0.1 by YourUsername"}
+    headers = {"Authorization": "bearer "+ token, "User-Agent": "conary/0.1"}
+    #print(headers)
     response = requests.get(url, headers=headers)
+    #print(response)
     data = json.loads(response.text)
- 
+    
     return data
+
+def get_subreddit(token, url, query):
+    post_data = {"exact": "false", "include_over_18": "true", "include_unadvertisable": "false", "query":query} 
+    headers = {"Authorization": "bearer "+ token, "User-Agent": "conary/0.1"}
+    response = requests.post(url, data=post_data, headers=headers)
+    cred = json.loads(response.text)
+    #print(cred)
+    return cred
+
+
+def get_posts(token, url):
+    headers = {"Authorization": "bearer "+ token, "User-Agent": "conary/0.1"}
+    #print(headers)
+    response = requests.get(url, headers=headers)
+    #print(response)
+    datas = json.loads(response.text)
+    json_data = json.dumps(datas['data']['children'], indent=4, sort_keys=True)
+    
+    # data_all = json_data['data']['children']
+    # print(data_all)
+    # num_of_posts = 0
+    # while len(data_all) <= 100:
+    #     time.sleep(2)
+    #     last = data_all[-1]['data']['name']
+    #     urls = 'https://www.reddit.com/r/politics/.json?after=' + str(last)
+    #     req = requests.get(urls, headers=headers)
+    #     data = json.loads(req.text)
+    #     data_all += data['data']['children']
+    #     if num_of_posts == len(data_all):
+    #         break
+    #     else:
+    #         num_of_posts = len(data_all)
+    #return data_all
+    return json_data
+
+
+
+
+
+
 
 
 token = get_token()
-datas = get_data(token, 'https://oauth.reddit.com/api/v1/me/prefs')
+datas = get_data(token, 'https://oauth.reddit.com/api/v1/me/')
 print (datas)
+subs = get_subreddit(token, 'https://oauth.reddit.com/api/search_reddit_names', 'trump')
+print (subs)
+top_data = get_posts(token, 'https://oauth.reddit.com/r/politics/.json')
+print(top_data)
 
 
 
